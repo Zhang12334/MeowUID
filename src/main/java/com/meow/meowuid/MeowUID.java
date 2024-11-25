3561fd43a0101010e529b8c317073755bdce0276
Zhang12334 <3143448821@qq.com>
2024-11-26 00:03:43 +0800

    fix

--- src/main/java/com/meow/meowuid/MeowUID.java
+++ src/main/java/com/meow/meowuid/MeowUID.java
@@ -12,7 +12,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.scheduler.BukkitRunnable;
-import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import me.clip.placeholderapi.PlaceholderAPI;
+import com.meow.meowuid.MeowUIDPlaceholderExpansion;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
